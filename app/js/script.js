ca2d882bfc355888898e127ccdc25597f892d9b2
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-01-17 14:48:37 +0100

    fade-in fade-out animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,24 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelector('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
     
     if(header.classList.contains('open')){
-        header.classList.remove('open')
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
     else {
-        header.classList.add('open'); 
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove ('fade-out');
+            element.classList.add ('fade-in');
+        });
     }
 
 });
